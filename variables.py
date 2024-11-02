001911fad653c69e2dfcd3e00309af4dd2203480
P1t1e <rkankulov01@gmail.com>
2024-11-02 23:37:48 +0300

    Переменные

--- variables.py
+++
@@ -1,5 +0,0 @@
-homework = 12
-hour = 1.5
-Course_name = 'Python,'
-time_for_one_task = (hour / homework)
-print('Курс:', Course_name, 'всего задач:', homework, ', затрачено часов', hour, ', среднее время выполнения', time_for_one_task, 'часа.')
